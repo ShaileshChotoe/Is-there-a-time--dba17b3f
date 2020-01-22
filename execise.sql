11736d4936c13d3e29e62cac97b528f6eed524ad
unknown <2120367@talnet.nl>
2020-01-22 13:26:09 +0100

    Uitwerking van de exercise

---
+++ execise.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS Sterrenstelsel;
+
+CREATE DATABASE Sterrenstelsel;
+
+USE Sterrenstelsel;
+
+CREATE TABLE Planeten (
+naam varchar(20) not null,
+diameter int(10) not null,
+afstand_zon int(10) not null,
+massa int(10) not null,
+bezoek_datum datetime(6)  
+);
+
+INSERT INTO Planeten (naam, diameter, afstand_zon, massa, bezoek_datum)
+VALUES ('aarde', 198, 681, 125, '2019-01-02');
+
+SELECT * FROM Planeten;
